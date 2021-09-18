0c7173cc6bb3c0d2e4848675ba6298c4a5acb9b6
e1630m <e1630m@gmail.com>
2021-09-18 12:05:03 +0000

    Add problem 12 solutions

---
+++ p0012.py
@@ -0,0 +1,36 @@
+def sprime(n, memo):
+    try:
+        return memo[n]
+    except KeyError:
+        for i in range(2, int(n**0.5 + 1)):
+            if not n % i:
+                memo[n] = i
+                return i
+        memo[n] = n
+        return n
+
+
+def get_factors(n, memo):
+    factors, prime = [], 2
+    while n >= prime:
+        prime = sprime(n, memo)
+        if n >= prime:
+            n //= prime
+            factors.append(prime)
+    return factors
+
+
+def count_divisors(factors):
+    prod = 1
+    for v in [factors.count(u) + 1 for u in list(set(factors))]:
+        prod *= v
+    return prod
+
+
+get_tnum = lambda x: (x**2 + x) // 2
+memo, i, triangle = {}, 1, get_tnum(1)
+while count_divisors(get_factors(triangle, memo)) < 500:
+    i += 1
+    triangle = get_tnum(i)
+print(get_tnum(i))
+    
