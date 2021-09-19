f5a837a065ea808fd1c92ac9961762179e04de2f
e1630m <e1630m@gmail.com>
2021-09-19 08:21:15 +0000

    Update Python solution for p1

--- p0001.py
+++ p0001.py
@@ -1,1 +1,15 @@
-print(sum(i for i in range(1000) if i % 3 == 0 or i % 5 == 0))
+from time import perf_counter_ns
+
+
+def gsum(n, d):
+	q, r = divmod(n, d)
+	return d * q * (q + 1) // 2 if r else d * q * (q - 1) // 2
+
+n = 1000
+iterations = int(1e6)
+
+start = perf_counter_ns()
+for _ in range(iterations):
+	ans = gsum(n, 3) + gsum(n, 5) - gsum(n, 15)
+end = perf_counter_ns()
+print(f'result: {ans}, time_elapsed: {end - start}, iterations: {iterations}')
